c03ec917217b701304d4a323045b15f491fddaca
khya31 <310464679+khya31@users.noreply.github.com>
2026-08-03 20:14:09 +0800

    Add day-two lunch itinerary for routes 3 and 8

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
-  appVersion: '2026.08.03-r27',
+  appVersion: '2026.08.03-r28',
   apiUrl: 'https://script.google.com/macros/s/AKfycbxGp7gwzzWHHBCwPN801EgjBTOwYYGRWCv91uS54zY9zn8O5RBouGuj38JsQNFimYHxjw/exec',
   apiTimeoutMs: 60000
 });
