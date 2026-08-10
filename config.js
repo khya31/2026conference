86b9fef781ec20840e9cdaaa8908b8c3a4025c80
khya31 <kypd2008@gmail.com>
2026-08-10 12:13:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = Object.freeze({
   appVersion: '2026.08.10-r52',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxguu0tGaZWrQ4l3t-wQVWs2ieAm7kxhAkle5GP3Xpymv3HGqcQ8ZSIrh6fyMTdIk03cA/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxGp7gwzzWHHBCwPN801EgjBTOwYYGRWCv91uS54zY9zn8O5RBouGuj38JsQNFimYHxjw/exec',
   apiTimeoutMs: 60000
 });
